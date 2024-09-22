152c669618d2953e0ac425cb8afcb5b1feb948cd
ofesyle <145034268+ofesyle@users.noreply.github.com>
2024-09-23 00:03:07 +0800

    Add files via upload

---
+++ src/main/java/wallyland/wallylandvacationplanner/PurchaseService.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package wallyland.wallylandvacationplanner.service;
+
+/**
+ * Handles the purchasing of tickets, food, and drinks
+ * This service processes transactions and updates inventory
+ */
+public class PurchaseService {
+    
+    /**
+     * @param userId The ID of the user making the purchase
+     * @param itemType The type of item being purchased (TICKET, FOOD, ETC...)
+     * @param itemId The ID of the item being purchased
+     * @param quantity The number of items to purchase
+     * @return true if successful
+     */
+    
+    public boolean processPurchase(String userId, String itemType, String itemId, int quantity) {
+        //imlepmentation
+        return true;
+    }
+    
+}
