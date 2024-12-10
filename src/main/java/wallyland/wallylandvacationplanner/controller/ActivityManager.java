fda0aabe919068955b442344aa3017f0e30c9d72
Skyler A/B <117874976+sklrthemighty@users.noreply.github.com>
2024-12-10 16:47:51 +0900

    Update ActivityManager.java

--- src/main/java/wallyland/wallylandvacationplanner/controller/ActivityManager.java
+++ src/main/java/wallyland/wallylandvacationplanner/controller/ActivityManager.java
@@ -1,46 +1,131 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package wallyland.wallylandvacationplanner.controller;
 
 import wallyland.wallylandvacationplanner.model.Activity;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- *
- * @author njthe
+ * The ActivityManager class is responsible for managing a list of activities,
+ * allowing activities to be added, removed, and updated.
+ * The activities are now managed using a HashMap.
  */
-/**
-     * The ActivityManager class is responsible for managing a list of activities,
-     * allowing activities to be added or removed.
+public class ActivityManager {
+    private Map<String, Activity> activities;
+
+    public ActivityManager() {
+        activities = new HashMap<>();
+        initializeActivities(); // Add some sample activities
+    }
+
+    /**
+     * Initializes some sample activities.
      */
-    public class ActivityManager {
-        private List<Activity> activities;
+    private void initializeActivities() {
+        addActivity(new Activity("R001", "Rollercoaster", "9AM-10PM", 30));
+        addActivity(new Activity("R002", "Other Attraction", "10AM-9PM", 40));
+        addActivity(new Activity("R003", "Ferris Wheel", "9AM-8PM", 50));
+    }
 
-        /**
-         * Constructs an ActivityManager instance and initializes the list of activities.
-         */
-        public ActivityManager() {
-            activities = new ArrayList<>();
+    /**
+     * Adds an activity to the list.
+     * 
+     * @param activity the activity to be added
+     */
+    public void addActivity(Activity activity) {
+        activities.put(activity.getActivityId(), activity);
+    }
+
+    /**
+     * Removes an activity from the list.
+     * 
+     * @param activity the activity to be removed
+     */
+    public void removeActivity(Activity activity) {
+        activities.remove(activity.getActivityId());
+    }
+
+    /**
+     * Retrieves an activity by its ID.
+     * 
+     * @param activityId the ID of the activity to retrieve
+     * @return the activity associated with the ID, or null if not found
+     */
+    public Activity getActivity(String activityId) {
+        return activities.get(activityId);
+    }
+
+    /**
+     * Returns a list of all activities.
+     * 
+     * @return a list of all activities
+     */
+    public List<Activity> getAllActivities() {
+        return new ArrayList<>(activities.values());
+    }
+
+    /**
+     * Schedules an activity by its ID.
+     * 
+     * @param activityId the ID of the activity to schedule
+     * @return true if the activity was scheduled successfully, false otherwise
+     */
+    public boolean scheduleActivity(String activityId) {
+        Activity activity = activities.get(activityId);
+        if (activity != null) {
+            return activity.scheduleActivity();
         }
+        return false;
+    }
 
-        /**
-         * Adds an activity to the list of managed activities.
-         * 
-         * @param activity the activity to be added
-         */
-        public void addActivity(Activity activity) {
-            activities.add(activity);
-        }
-
-        /**
-         * Removes an activity from the list of managed activities.
-         * 
-         * @param activity the activity to be removed
-         */
-        public void removeActivity(Activity activity) {
-            activities.remove(activity);
+    /**
+     * Cancels an activity by its ID.
+     * 
+     * @param activityId the ID of the activity to cancel
+     */
+    public void cancelActivity(String activityId) {
+        Activity activity = activities.get(activityId);
+        if (activity != null) {
+            activity.cancelActivity();
         }
     }
+
+    /**
+     * Updates the details of an activity.
+     * 
+     * @param activity the activity to update
+     */
+    public void updateActivity(Activity activity) {
+        if (activities.containsKey(activity.getActivityId())) {
+            activities.put(activity.getActivityId(), activity);
+        } else {
+            System.out.println("Activity with ID " + activity.getActivityId() + " does not exist.");
+        }
+    }
+
+    /**
+     * Retrieves an activity by its ID. Same as getActivity, but named for clarity.
+     * 
+     * @param activityId the ID of the activity to retrieve
+     * @return the activity with the specified ID, or null if not found
+     */
+    public Activity getActivityById(String activityId) {
+        return activities.get(activityId);
+    }
+
+    /**
+     * Returns a list of all scheduled activities.
+     * 
+     * @return a list of all scheduled activities
+     */
+    public List<Activity> getScheduledActivities() {
+        List<Activity> scheduledActivities = new ArrayList<>();
+        for (Activity activity : activities.values()) {
+            if (activity.isScheduled()) {
+                scheduledActivities.add(activity);
+            }
+        }
+        return scheduledActivities;
+    }
+}
