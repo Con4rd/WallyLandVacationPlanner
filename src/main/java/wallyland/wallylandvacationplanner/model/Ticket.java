cd4e50072003be9386d316327ba6412f11df0849
Skyler A/B <117874976+sklrthemighty@users.noreply.github.com>
2024-12-10 16:56:15 +0900

    Update Ticket.java

--- src/main/java/wallyland/wallylandvacationplanner/model/Ticket.java
+++ src/main/java/wallyland/wallylandvacationplanner/model/Ticket.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package wallyland.wallylandvacationplanner.model;
 
 /**
@@ -11,50 +7,23 @@ package wallyland.wallylandvacationplanner.model;
 public class Ticket {
     private String ticketId;
     private String type;
-    private boolean isValid;
     private double price;
+    private boolean isValid;
 
-    /**
-     * Constructs a new Ticket.
-     *
-     * @param ticketId The unique identifier of each ticket.
-     * @param type     The type of the ticket (e.g., day pass, annual pass).
-     * @param price    The price of the ticket.
-     * @param isValid  The validity of the ticket.
-     */
-    public Ticket(String ticketId, String type,double price, boolean isValid) {
+    public Ticket(String ticketId, String type, double price, boolean isValid) {
         this.ticketId = ticketId;
         this.type = type;
         this.price = price;
         this.isValid = isValid;
     }
 
-    //   Getters and setters
-    public String getTicketId() {
-        return ticketId;
-    }
+    public String getTicketId() { return ticketId; }
+    public String getType() { return type; }
+    public double getPrice() { return price; }
+    public boolean isValid() { return isValid; }
 
-    public void setTicketId(String ticketId) {
-        this.ticketId = ticketId;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-     @Override
+    @Override
     public String toString() {
-        return ticketId + " " + type + " -- $" + String.format("%.2f", price) + isValid;
+        return type + " Ticket ($" + String.format("%.2f", price) + ") " + (isValid ? "Available" : "Out of Stock");
     }
 }
