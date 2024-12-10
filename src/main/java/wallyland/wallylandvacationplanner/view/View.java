73bdc7b19bdfd2f5afbff0816951440ded3c9c71
Skyler A/B <117874976+sklrthemighty@users.noreply.github.com>
2024-12-10 17:00:13 +0900

    Update View.java

--- src/main/java/wallyland/wallylandvacationplanner/view/View.java
+++ src/main/java/wallyland/wallylandvacationplanner/view/View.java
@@ -1,27 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package wallyland.wallylandvacationplanner.view;
 
-/**
- *
- * @author chris
- */
+import wallyland.wallylandvacationplanner.view.UserInterface;
+import wallyland.wallylandvacationplanner.view.AdminInterface;
+import wallyland.wallylandvacationplanner.model.PurchaseService;
+
 
 public class View {
     private UserInterface userInterface;
     private AdminInterface adminInterface;
-    
-    public View() {
-        userInterface = new UserInterface();
+
+    public View(PurchaseService purchaseService) {
+        userInterface = new UserInterface(purchaseService); // Pass the PurchaseService here
         adminInterface = new AdminInterface();
     }
-    
+
     public UserInterface getUserInterface() {
         return userInterface;
     }
-    
+
     public AdminInterface getAdminInterface() {
         return adminInterface;
     }
