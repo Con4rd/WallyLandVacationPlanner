0dc8bcbed895b6d907045e9375ad13e9343018a3
Skyler A/B <117874976+sklrthemighty@users.noreply.github.com>
2024-12-10 16:55:46 +0900

    Update Statistics.java

--- src/main/java/wallyland/wallylandvacationplanner/model/Statistics.java
+++ src/main/java/wallyland/wallylandvacationplanner/model/Statistics.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package wallyland.wallylandvacationplanner.model;
 
-/**
- *
- * @author njthe
- */
 /**
  * The Statistics class maintains and provides access to visitor and sales data.
  */
