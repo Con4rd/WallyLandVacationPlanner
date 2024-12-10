5af4e920c879592efc836df572cbb283a7e895c7
Skyler A/B <117874976+sklrthemighty@users.noreply.github.com>
2024-12-10 17:01:42 +0900

    Update WallyLandVacationPlanner.java

--- src/main/java/wallyland/wallylandvacationplanner/WallyLandVacationPlanner.java
+++ src/main/java/wallyland/wallylandvacationplanner/WallyLandVacationPlanner.java
@@ -3,6 +3,7 @@ package wallyland.wallylandvacationplanner;
 import wallyland.wallylandvacationplanner.controller.Controller;
 import wallyland.wallylandvacationplanner.model.Model;
 import wallyland.wallylandvacationplanner.view.View;
+import wallyland.wallylandvacationplanner.model.PurchaseService;
 
 public class WallyLandVacationPlanner {
     private Model model;
@@ -11,8 +12,9 @@ public class WallyLandVacationPlanner {
     
     public WallyLandVacationPlanner() {
         model = new Model();
-        view = new View();
-        controller = new Controller(model, view);
+        PurchaseService purchaseService = new PurchaseService(); // Initialize PurchaseService
+        view = new View(purchaseService); // Pass PurchaseService to View
+        controller = new Controller(model, purchaseService); // Pass PurchaseService to Controller
         
         // Make the main window visible
         view.getUserInterface().setVisible(true);
